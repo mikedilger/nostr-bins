a580d4a3daeb92c80d315923a26ee61ce0a6fc32
Mike Dilger <mike@mikedilger.com>
2023-10-09 08:04:44 +1300

    fetch_by_kind_and_author

---
+++ src/bin/fetch_by_kind_and_author.rs
@@ -0,0 +1,37 @@
+
+use std::env;
+use nostr_types::{EventKind, Filter, PublicKey, PublicKeyHex};
+
+fn main() {
+    let mut args = env::args();
+    let _ = args.next(); // program name
+    let relay_url = match args.next() {
+        Some(u) => u,
+        None => panic!("Usage: fetch_by_kind_and_author <RelayURL> <KindNumber> <PubKey>"),
+    };
+    let kind_number = match args.next() {
+        Some(num) => num.parse::<u32>().unwrap(),
+        None => panic!("Usage: fetch_by_kind_and_author <RelayURL> <KindNumber> <PubKey>"),
+    };
+    let kind: EventKind = kind_number.into();
+    let author_key = match args.next() {
+        Some(key) => match PublicKey::try_from_bech32_string(&key, true) {
+            Ok(key) => key,
+            Err(_) => match PublicKey::try_from_hex_string(&key, true) {
+                Ok(key) => key,
+                Err(_) => panic!("Could not parse public key"),
+            }
+        },
+        None => panic!("Usage: fetch_by_kind_and_author <RelayURL> <KindNumber> <PubKey>"),
+    };
+    let key: PublicKeyHex = author_key.into();
+    let filter = Filter {
+        kinds: vec![kind],
+        authors: vec![key.prefix(32)],
+        .. Default::default()
+    };
+
+    for event in nostr_bins::fetch_by_filter(&relay_url, filter) {
+        nostr_bins::print_event(&event);
+    }
+}
