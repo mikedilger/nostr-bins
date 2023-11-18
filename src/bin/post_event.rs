bdf8cb81da36d99a7edf358469dcf0afeea5e9c5
Mike Dilger <mike@mikedilger.com>
2023-11-19 11:01:07 +1300

    post_event binary

---
+++ src/bin/post_event.rs
@@ -0,0 +1,20 @@
+
+use std::env;
+use std::io::Read;
+use nostr_types::Event;
+
+fn main() {
+    let mut args = env::args();
+    let _ = args.next(); // program name
+    let relay_url = match args.next() {
+        Some(u) => u,
+        None => panic!("Usage: fetch_by_id <RelayURL> < EventJSON"),
+    };
+
+    let mut s: String = String::new();
+    std::io::stdin().read_to_string(&mut s).unwrap();
+
+    let event: Event = serde_json::from_str(&s).unwrap();
+
+    nostr_bins::post_event(&relay_url, event);
+}
