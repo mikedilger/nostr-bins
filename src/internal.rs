babb8c586e7339c2bba843d73dd703d7150b84dd
Mike Dilger <mike@mikedilger.com>
2023-11-19 10:58:50 +1300

    Internal code to post an event

--- src/internal.rs
+++ src/internal.rs
@@ -9,6 +9,11 @@ pub(crate) fn filters_to_wire(filters: Vec<Filter>) -> String {
     serde_json::to_string(&message).expect("Could not serialize message")
 }
 
+pub(crate) fn event_to_wire(event: Event) -> String {
+    let message = ClientMessage::Event(Box::new(event));
+    serde_json::to_string(&message).expect("Could not serialize message")
+}
+
 pub(crate) fn fetch(host: String, uri: Uri, wire: String) -> Vec<Event> {
 
     let mut events: Vec<Event> = Vec::new();
@@ -92,4 +97,62 @@ pub(crate) fn fetch(host: String, uri: Uri, wire: String) -> Vec<Event> {
     }
 
     events
+}
+
+pub(crate) fn post(host: String, uri: Uri, wire: String) {
+    let key: [u8; 16] = rand::random();
+    let request = http::request::Request::builder()
+        .method("GET")
+        .header("Host", host)
+        .header("Connection", "Upgrade")
+        .header("Upgrade", "websocket")
+        .header("Sec-WebSocket-Version", "13")
+        .header(
+            "Sec-WebSocket-Key",
+            base64::engine::general_purpose::STANDARD.encode(key),
+        )
+        .uri(uri)
+        .body(())
+        .expect("Could not build request");
+
+    let (mut websocket, _response) =
+        tungstenite::connect(request).expect("Could not connect to relay");
+
+    websocket
+        .write_message(Message::Text(wire))
+        .expect("Could not send message to relay");
+
+    // Get and print one response message
+
+    let message = match websocket.read_message() {
+        Ok(m) => m,
+        Err(e) => {
+            println!("Problem reading from websocket: {}", e);
+            return;
+        }
+    };
+
+    match message {
+        Message::Text(s) => {
+            let relay_message: RelayMessage =
+                serde_json::from_str(&s).expect(&s);
+            match relay_message {
+                RelayMessage::Event(_, e) => println!("EVENT: {}", serde_json::to_string(&e).unwrap()),
+                RelayMessage::Notice(s) => println!("NOTICE: {}", s),
+                RelayMessage::Eose(_) => println!("EOSE"),
+                RelayMessage::Ok(_id, ok, reason) => println!("OK: ok={} reason={}", ok, reason),
+                RelayMessage::Auth(challenge) => println!("AUTH: {}", challenge),
+            }
+        }
+        Message::Binary(_) => println!("IGNORING BINARY MESSAGE"),
+        Message::Ping(vec) => if let Err(e) = websocket.write_message(Message::Pong(vec)) {
+            println!("Unable to pong: {}", e);
+        }
+        Message::Pong(_) => println!("IGNORING PONG"),
+        Message::Close(_) => {
+            println!("Closing");
+            return;
+        }
+        Message::Frame(_) => println!("UNEXPECTED RAW WEBSOCKET FRAME"),
+    }
 }
