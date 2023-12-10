47bd319d3c06d5221aa7c94f06adb48207e96eca
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-12-10 12:31:01 -0500

    src/internal.rs

--- src/internal.rs
+++ src/internal.rs
@@ -54,6 +54,7 @@ pub(crate) fn fetch(host: String, uri: Uri, wire: String) -> Vec<Event> {
                 let relay_message: RelayMessage =
                     serde_json::from_str(&s).expect(&s);
                 match relay_message {
+                    RelayMessage::Closed(_, _) => todo!(),
                     RelayMessage::Event(_, e) => events.push(*e),
                     RelayMessage::Notice(s) => println!("NOTICE: {}", s),
                     RelayMessage::Eose(_) => {
@@ -142,6 +143,7 @@ pub(crate) fn post(host: String, uri: Uri, wire: String) {
                 RelayMessage::Eose(_) => println!("EOSE"),
                 RelayMessage::Ok(_id, ok, reason) => println!("OK: ok={} reason={}", ok, reason),
                 RelayMessage::Auth(challenge) => println!("AUTH: {}", challenge),
+                RelayMessage::Closed(_, _) => todo!(),
             }
         }
         Message::Binary(_) => println!("IGNORING BINARY MESSAGE"),
